36f7eed65951882b7664cc1f72505c0c196e95fe
sookool99 <shawnmacburnie@gmail.com>
2014-11-16 17:32:25 -0400

    initial project setup

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name selfStatsApp
+ * @description
+ * # selfStatsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('selfStatsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'restangular',
+    'ui.bootstrap',
+    'ui.router'
+  ])
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name selfStatsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the selfStatsApp
+ */
+angular.module('selfStatsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
